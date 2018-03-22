5bedf3c7fdd16c0d3639d0c2daa9505e05f5d7f2
Sebastian Palm <sebastian.palm91@gmail.com>
2018-03-22 13:34:24 +0100

    made connection to db with fetch also adding to db cart with productid

--- pages/products.php
+++ pages/products.php
@@ -1,31 +1,5 @@
 <?php require __DIR__.'/../views/header.php'; ?>
-<div class="d-flex justify-content-center row">
-  <div class="products">
-    <div class="nike1">
-
-    </div>
-    <button type="button" name="button">Add to cart!</button>
-  </div>
-
-  <div class="products">
-    <div class="nike2">
-
-    </div>
-    <button type="button" name="button">Add to cart!</button>
-  </div>
-
-  <div class="products">
-    <div class="ralph">
-
-    </div>
-    <button type="button" name="button">Add to cart!</button>
-  </div>
-
-  <div class="products">
-    <div class="vans">
-
-    </div>
-    <button type="button" name="button">Add to cart!</button>
-  </div>
+<div class="d-flex justify-content-center row productscontainer">
 </div>
+
 <?php require __DIR__.'/../views/footer.php'; ?>
--- source/scripts/products.js
+++ source/scripts/products.js
@@ -1,21 +1,49 @@
-const productsLists = document.querySelectorAll('.products');
+//
+// * Fetcing API from db
+// * Get objects
+// * Adding value to API
+//
 
-const url = "http://localhost:5000/api/product";
-
-Array.from(productsLists).forEach(productsList => {
-  productsList.addEventListener('load', () => {
-    let products = productsList.value;
-    fetch(url)
-    .then(response => {
-      return response.json()
-    })
-    .then(response => {
-      console.log(response);
-      // var newDiv = document.createElement("div");
-      // var newContent = document.createTextNode(response.length);
-      // newDiv.appendChild(newContent);
-      // var currentDiv = document.getElementsByClassName('products');
-      // document.body.insertBefore(newDiv, currentDiv);
-    })
+fetch('http://localhost:5000/api/product', {
+  method: 'GET', // or 'PUT'
+  headers: new Headers({
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
   })
+}).then(res => res.json())
+.then(products => {
+  for (const product of products){
+
+    const  {id:productid, description:productdescription, name:productname, price:productprice, slug:productslug, productImage:productimage} = product
+    var productsList = document.querySelector('.productscontainer');
+
+    productsList.innerHTML +=  `<div class="products">
+    <img class="img" src="${productimage}" alt="">
+    <p>${productname}</p>
+    <p>${productdescription}</p>
+    <p>Price: ${productprice}$</p>
+    <button class="AddCartButton" type="submit" value="${productid}" name="button">Add to cart!</button>
+    </div>`
+
+    const AddCartButtons = document.querySelectorAll('.AddCartButton');
+
+    Array.from(AddCartButtons).forEach(AddCartButton => {
+      AddCartButton.addEventListener('click', () => {
+        const cart =
+        {
+          "cartid": "32", //Le cookie
+          "productid": AddCartButton.value
+        }
+        fetch('http://localhost:5000/api/cart', {
+          method: 'POST', // or 'PUT'
+          body: JSON.stringify(cart),
+          headers: new Headers({
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          })
+        }).then(res => res.json())
+        .then(response => console.log('Success:', response));
+      })
+    })
+  }
 });
